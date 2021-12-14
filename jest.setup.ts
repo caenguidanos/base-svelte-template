b23f64ad6a054fadd4ab6c1f2adbaaae69e1702e
Cristian Alejandro Enguídanos Nebot <53697395+caenguidanos@users.noreply.github.com>
2021-12-14 22:58:51 +0100

    refactor: change import

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,8 @@
-import "@testing-library/jest-dom/extend-expect";
 import fetch from "isomorphic-fetch";
 
 import { server } from "$msw/server";
+
+import "@testing-library/jest-dom/extend-expect";
 
 export function loadIsomorphicFetch(): void {
    globalThis.fetch = fetch as unknown as typeof globalThis.fetch;
